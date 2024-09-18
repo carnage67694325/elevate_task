1f2dff834b52fbc56043b218fcde9dd59d9aa395
carnage67694325 <kingabdoeid2014@gmail.com>
2024-09-18 15:22:09 +0300

    strucutred the project and created home view

---
+++ lib/features/home/presentation/view/home_view.dart
@@ -0,0 +1,13 @@
+import 'package:elevate_task/features/home/presentation/view/widgets/home_view_body.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: HomeViewBody(),
+    );
+  }
+}

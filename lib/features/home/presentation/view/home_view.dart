ae3a7b747cd94267fd1b91ba889b00aee2b87001
carnage67694325 <kingabdoeid2014@gmail.com>
2024-09-18 17:10:54 +0300

    provided the cubit

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -1,8 +1,21 @@
+import 'package:elevate_task/features/home/presentation/view/view_model/cubit/get_product_cubit.dart';
 import 'package:elevate_task/features/home/presentation/view/widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  void initState() {
+    BlocProvider.of<GetProductCubit>(context).getProducts();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
+import 'package:dio/dio.dart';
+import 'package:elevate_task/core/utils/api_service.dart';
+import 'package:elevate_task/features/home/data/repos/home_repo.dart';
+import 'package:elevate_task/features/home/data/repos/home_repo_imp.dart';
 import 'package:elevate_task/features/home/presentation/view/home_view.dart';
+import 'package:elevate_task/features/home/presentation/view/view_model/cubit/get_product_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const StoreApp());
@@ -10,8 +16,12 @@ class StoreApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeView(),
+    return BlocProvider(
+      create: (context) =>
+          GetProductCubit(HomeRepoImp(apiService: ApiService(Dio()))),
+      child: const MaterialApp(
+        home: HomeView(),
+      ),
     );
   }
 }
